4b2e06052a941c0c15b5751a3c4b5f97575df02c
fishmanjohn <john@emmaandjohnbyvan.com>
2019-11-15 13:17:40 -0800

    vp

--- src/components/CharacterCard.js
+++ src/components/CharacterCard.js
@@ -1,14 +1,29 @@
 import React from "react";
+import styled from 'styled-components';
+
+ const SquanchCard = styled.div`
+ height:400px;
+ width:90%;
+ display:flex;
+ flex-direction:column;
+ justify-content: space-evenly;
+ align-items: center;
+ margin: 15px;
+ border: 3px solid green;
+ `;
+ const CharImg =styled.img`
+ height: 150px;
+ `
 
 export default function CharacterCard(props) {
 
-  return<span>
-      <div className ="teamCard">
+  return <span>
+      <SquanchCard>
           <h2>Name: {props.person.name}</h2>
-          <image src={props.person.image}/>
+          <CharImg alt='' src={props.person.image}/>
           <h2>Location: {props.person.location.name}</h2>
           <h2>Status: {props.person.status}</h2>
 
-      </div>
+      </SquanchCard>
       </span>
 }
--- src/components/CharacterList.js
+++ src/components/CharacterList.js
@@ -1,35 +1,45 @@
 import React, { useEffect, useState } from "react";
 import axios from 'axios';
-import CharacterCard from './CharacterCard'
-
+import CharacterCard from './CharacterCard';
+import SearchForm from './SearchForm';
 
 export default function CharacterList() {
   // TODO: Add useState to track data from useEffect
 const [character, setCharacter] = useState([])
+const [search,setSearch]= useState(character)
   useEffect(() => {
     // TODO: Add API Request here - must run in `useEffect`
     //  Important: verify the 2nd `useEffect` parameter: the dependancies array!
-    const getCharacters = ()=> {
+    
     axios.get('https://rickandmortyapi.com/api/character/')
     .then(res=>{
       setCharacter(res.data.results)})
 
     .catch(err=>{console.log(err.response);})
  
-    }
+    
  
-    getCharacters();
+   
     }, []);
     console.log(character)
-
+    const handleSearch = (input)=>{
+      console.log(input)
+      setCharacter(
+        character.filter(item =>{ 
+          return item.name.toLowerCase().includes(input.toLowerCase())
+        })
+      )
+    }
   return (
     <section className="character-list">
-     ( <div className = "grid-view">
+     {/* ( <div className = "grid-view">
     {character.map( (item, index) => {
         return <CharacterCard person = {item} key={index}/>
     })}
     </div>
-   )
+   ) */}
+   <SearchForm character={character} handleSearch={handleSearch}/>
+
     </section>
   );
 }
